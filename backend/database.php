93d469975fc4ded02fb9fe4c1b4951cd1f7b5f34
castelll0009 <castelll0009@gmail.com>
2021-08-11 13:36:40 -0500

    Conexcion a Base de datos remota clever cloud  segundo intento

--- backend/database.php
+++ backend/database.php
@@ -1,14 +1,13 @@
 <?php //gestina las peticiones que se hacen desde el request-app.js 
     //No conectamos a la base de datos
     $connection = mysqli_connect(
+        //host,user,pasword,nombre base datos,
         'bey7jfoaqc2cukiettfo-mysql.services.clever-cloud.com', 'uivq6qdjzpey0dej', 'dnfpmdT4NimRywJikAl', 'bey7jfoaqc2cukiettfo'
     );
 
-    
+    /*
     if($connection){
         echo "database is connected";
-    }   else{
-        echo "No pudismo conectarnos a la  base de datos";
-    }
-    
+    }   
+    */ 
 ?>
