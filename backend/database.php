396e57d124f3e669731815b3aa3fa917e30285c8
castelll0009 <castelll0009@gmail.com>
2021-08-11 14:57:15 -0500

    dev mysql

--- backend/database.php
+++ backend/database.php
@@ -2,7 +2,7 @@
     //No conectamos a la base de datos
     $connection = mysqli_connect(
         //host,user,pasword,nombre base datos,
-        'bey7jfoaqc2cukiettfo-mysql.services.clever-cloud.com', 'uivq6qdjzpey0dej', 'dnfpmdT4NimRywJikAl', 'bey7jfoaqc2cukiettfo'
+        'blzwwwafrtnoulyompah-mysql.services.clever-cloud.com', 'uflmxyisjbdu7w7l', 'k5ulb9EJWmdqIgYLjNHF', 'blzwwwafrtnoulyompah'
     );
 
     
