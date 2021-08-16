d589ff871537d2ed893475facf7483cd770bd65c
castelll0009 <castelll0009@gmail.com>
2021-08-15 22:24:25 -0500

    se agrego la notifiacion jgrowl , se configuro la conrimacion de compra
    cambiados:
    detalle_productos
    detalles_carrito
    index.html

--- js/detalles_producto.js
+++ js/detalles_producto.js
@@ -48,6 +48,7 @@ var aumentar_productos = document.querySelector(".aumentar-productos");
 /* comprar para los diferentes productos*/
 var boton_comprar = document.querySelector(".btn-ordenar");
 var boton_agregar = document.querySelector(".btn-agregar");
+var boton_confirmar_orden = document.querySelector(".btn-confirmar-orden");
 var titulos_productos = document.querySelector(".titulo-detalles-producto");
 var precios_productos = document.querySelector(".precio-detalles-producto");
   var descripcion_productos = document.querySelector("descripcion-detalles-producto");
@@ -64,60 +65,70 @@ var nombre_pedido;
 var cantidad_pedido;
 var precio_pedido;
 var counter = 0; // para el index del producto
-
 boton_agregar.addEventListener("click", function(){
-  event.stopPropagation(); 
-      
-  //recuperamos el Nombre,Cantidad y el precio del producto agregar al  pedidoo carrito
-  nombre_pedido = titulo_producto_vender_original; 
-  cantidad_pedido= cantidad_productos.value;
-  precio_pedido = precio_producto_vender; 
-  //creamos un nuevo template  tr que contendra los 3  valores        
-  //AGREGAR FILA CARRITO CALCULAR TOTALES  , agregar un elemento al carrito  
-  auxCantidad = cantidad_total_productos_vender;
-  auxTotal = parseInt(total_pagar_pedido);  
-  counter = addNewRow();    
-  function addNewRow(){
-   if(nombre_pedido != undefined && cantidad_pedido  != undefined &&  precio_pedido != undefined){
-      counter++;     
-      contElementosPedido++; 
-      if(contElementosPedido < 7){
-        //por ahora quitamos la propiedad contenteitable de la t cantiodadeditable contenteditable
-        $('.table').append(`<tr id="fila${counter}"><td> ${counter} 
-        </td><td> ${nombre_pedido}
-        </td><td  class="cantidadEditable"  onclick="cambiosCantidadPedido()" onkeypress="return (event.charCode >= 48 && event.charCode <= 57)">${cantidad_pedido}
-        </td><td> ${precio_pedido}
-        </td><td><i onclick="eliminarFilaPedido(${counter})" class="fas fa-trash-alt"></i></td></tr>`);      
-        auxCantidad +=  parseInt(cantidad_pedido);        
-        auxTotal += parseInt(cantidad_pedido) * parseInt(precio_pedido);
-        //calculamos los valores cantidad total y total pagar 
-        cantidad_total_productos_vender  = parseInt(auxCantidad);
-        total_pagar_pedido = auxTotal;      
-        document.getElementById("cantidad-total-pedido").innerHTML = parseInt(cantidad_total_productos_vender);
-        document.getElementById("total-pagar-pedido").innerHTML = total_pagar_pedido;
-      }else{      
-        contElementosPedido--;//producia un error que iba agotando el pedido  
-        alert('Limite de productos por factura alcanzado!, puedes crear otro pedido'); 
-      }
-        
-   }else{
-     alert("Su producto no ha sido agregado al carrito, Porfavor intentelo de nuevo ;)")
-   }    
-      return counter;      
-  }   
-  //funcion para 
+  event.stopPropagation();       
+  agregarProductoCarrito();
+
 });
 
-
+function agregarProductoCarrito(){     
+    //recuperamos el Nombre,Cantidad y el precio del producto agregar al  pedidoo carrito
+    nombre_pedido = titulo_producto_vender_original; 
+    cantidad_pedido= cantidad_productos.value;
+    precio_pedido = precio_producto_vender; 
+    //creamos un nuevo template  tr que contendra los 3  valores        
+    //AGREGAR FILA CARRITO CALCULAR TOTALES  , agregar un elemento al carrito  
+    auxCantidad = cantidad_total_productos_vender;
+    auxTotal = parseInt(total_pagar_pedido);  
+    counter = addNewRow();    
+    function addNewRow(){
+     if(nombre_pedido != undefined && cantidad_pedido  != undefined &&  precio_pedido != undefined){
+        counter++;     
+        contElementosPedido++; 
+        if(contElementosPedido < 7){
+          //por ahora quitamos la propiedad contenteitable de la t cantiodadeditable contenteditable
+          $('.table').append(`<tr id="fila${counter}"><td> ${counter} 
+          </td><td> ${nombre_pedido}
+          </td><td  class="cantidadEditable"  onclick="cambiosCantidadPedido()" onkeypress="return (event.charCode >= 48 && event.charCode <= 57)">${cantidad_pedido}
+          </td><td> ${precio_pedido}
+          </td><td><i onclick="eliminarFilaPedido(${counter})" class="fas fa-trash-alt"></i></td></tr>`);      
+          auxCantidad +=  parseInt(cantidad_pedido);        
+          auxTotal += parseInt(cantidad_pedido) * parseInt(precio_pedido);
+          //calculamos los valores cantidad total y total pagar 
+          cantidad_total_productos_vender  = parseInt(auxCantidad);
+          total_pagar_pedido = auxTotal;      
+          document.getElementById("cantidad-total-pedido").innerHTML = parseInt(cantidad_total_productos_vender);
+          document.getElementById("total-pagar-pedido").innerHTML = total_pagar_pedido;          
+          $.jGrowl(`Has agregado ${nombre_pedido} al carrito de compras`,{life : 2000});
+        }else{      
+          contElementosPedido--;//producia un error que iba agotando el pedido  
+          alert('Limite de productos por factura alcanzado!, puedes crear otro pedido'); 
+        }
+          
+     }else{
+       alert("Su producto no ha sido agregado al carrito, Porfavor intentelo de nuevo ;)")
+     }    
+        return counter;      
+    }   
+    //funcion para   
+}
 //CUANDO PRESIOAMOS EN ORDENAR
 boton_comprar.addEventListener("click", function(){  
    //enviamos el pedido a whatsap   
    //al hacer el pedido se despliega la factura o carrito con el pedido del cliente
    $(".div-detalles-carrito").toggleClass("mostrar-detalles-carrito");     
+   agregarProductoCarrito();
+
     alert("Observa el carrito de compras antes de confirmar tu orden");
-      //boton Confirmar Orden
-    document.getElementsByClassName("btn-concretar-orden").addEventListener("click", function(){
-      if(confirm("¿Estas seguro que deseas finalizar tu compra?, tu pedido sera enviado al Whatsapp del vendedor.") ){
+      //boton Confirmar Orden    
+     
+ 
+
+});
+
+//CONFIRMAR ORDEN
+boton_confirmar_orden.addEventListener("click", function(){  
+   if(confirm("¿Estas seguro que deseas finalizar tu compra?, tu pedido sera enviado al Whatsapp del vendedor.") ){
         //el usuario acepto hacer la compra, recuperamos los datos de su pedido           
         /*
         titulo_producto_vender = titulos_productos.innerHTML.toUpperCase();     
@@ -151,9 +162,7 @@ boton_comprar.addEventListener("click", function(){
         cadenaURL = "https://wa.me/"+numero_telefono+"?text=🍺🍸🍨%0AMOJITOS (PEDIDO ONLINE)%0A🍺🍸🍨%0A%0A"+titulo_producto_vender+"%0APRECIO:$ "+precio_producto_vender+"%0ADESCRIPCIÓN:"+
         descripcion_producto_vender+"%0ACANTIDAD: "+cantidad_producto_vender+"%0ATOTAL%20A%20PAGAR>> $ "+TOTAL_PAGAR_producto_vender+"%0A%0A->>Pango pendiente<<-";
         URL_orden =  window.location= cadenaURL;   
-      }
-    });
- 
+      }    
 
 });
 //activamos el swiper slide despues de enlistar los productos, evitamos error de funcionalidad
